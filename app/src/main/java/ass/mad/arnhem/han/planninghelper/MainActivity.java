5fa2263ead0807d6f3a843e81a315aacd4e0dea7
Peter Otten <peterotten128@hotmail.com>
2016-05-21 13:22:58 +0200

    fix ignore and conflicts

--- app/src/main/java/ass/mad/arnhem/han/planninghelper/MainActivity.java
+++ app/src/main/java/ass/mad/arnhem/han/planninghelper/MainActivity.java
@@ -1,15 +1,10 @@
 package ass.mad.arnhem.han.planninghelper;
 
-<<<<<<< HEAD
-import android.app.Fragment;
-=======
->>>>>>> 1b51f83584399b47d98841c9288f6e4d65fe183c
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
