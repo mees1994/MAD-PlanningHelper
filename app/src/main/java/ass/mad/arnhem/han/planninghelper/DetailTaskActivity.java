e7b9bebe58574c4fd3a7bf3fa694eb852e17f8e4
Mees <Mees_ajax@hotmail.com>
2016-05-25 11:03:42 +0200

    forgot to commit

--- app/src/main/java/ass/mad/arnhem/han/planninghelper/DetailTaskActivity.java
+++ app/src/main/java/ass/mad/arnhem/han/planninghelper/DetailTaskActivity.java
@@ -5,7 +5,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import ass.mad.arnhem.han.planninghelper.Domain.Task;
 import ass.mad.arnhem.han.planninghelper.Domain.Week;
@@ -13,6 +16,8 @@ import ass.mad.arnhem.han.planninghelper.Domain.Week;
 public class DetailTaskActivity extends AppCompatActivity {
 
     private int dayNr, taskPos;
+    private TextView taskTitle, taskDescription, taskStartTime, taskEndTime;
+    private ImageView taskIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +34,19 @@ public class DetailTaskActivity extends AppCompatActivity {
             dayNr = extras.getInt("dayNr");
         }
 
+        taskTitle = (TextView) findViewById(R.id.detailsTitle);
+        taskDescription = (TextView) findViewById(R.id.detailsDescription);
+        taskStartTime = (TextView) findViewById(R.id.detailsStartTime);
+        taskEndTime = (TextView) findViewById(R.id.detailsEndTime);
+        taskIcon = (ImageView) findViewById(R.id.detailIcon);
 
-        getSupportActionBar().setTitle(week.getDays().get(dayNr).getTasks().get(taskPos).getTitel());
+        Task task = week.getDays().get(dayNr).getTasks().get(taskPos);
 
+        taskTitle.setText(task.getTitel());
+        taskDescription.setText(task.getDescription());
+        taskStartTime.setText(task.getStartTime());
+        taskEndTime.setText(task.getEndTime());
+        taskIcon.setImageDrawable(getDrawable(task.getIcon()));
 
     }
 
